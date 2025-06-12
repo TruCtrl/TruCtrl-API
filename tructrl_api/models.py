86714d0a7cdbd33e368a0c2479af28bb14be0349
Nathan C. McGuire <nathan@ncm.me>
2025-06-12 16:05:59 -0500

    Update models.py

--- tructrl_api/models.py
+++ tructrl_api/models.py
@@ -1,1 +1,16 @@
-from .meraki.organizations.models import MerakiOrganization
+# File:     models.py
+# Package:  meraki
+# Package:  tructrl_api
+# Project:  TruCtrl
+
+
+# --- Imports ---
+
+# Standard Imports
+
+# Third-Party Imports
+
+# Project Imports
+
+# Module Imports
+from .meraki import Organization as MerakiOrganization
